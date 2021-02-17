a2dd67390254d1a141e8dca9e27738f513eb4d7f
mnstucky <mnstucky@gmail.com>
2021-02-17 17:03:58 -0600

    Add category functionality

--- add_todo.php
+++ add_todo.php
@@ -4,12 +4,14 @@ require_once('db_connect.php');
 
 $new_todo_title = filter_input(INPUT_POST, 'new_todo_title');
 $new_todo_description = filter_input(INPUT_POST, 'new_todo_description');
+$new_todo_categoryId = filter_input(INPUT_POST, 'new_todo_categoryId', FILTER_VALIDATE_INT);
 
-$insertTodoQuery = 'INSERT INTO todoitems (title, description)
-                VALUES (:new_todo_title, :new_todo_description)';     
+$insertTodoQuery = 'INSERT INTO todoitems (title, description, categoryid)
+                VALUES (:new_todo_title, :new_todo_description, :new_todo_categoryId)';     
 $insertTodoStatement = $db->prepare($insertTodoQuery);
 $insertTodoStatement->bindValue(':new_todo_title', $new_todo_title);
 $insertTodoStatement->bindValue(':new_todo_description', $new_todo_description);
+$insertTodoStatement->bindValue(':new_todo_categoryId', $new_todo_categoryId);
 $insertTodoStatement->execute();
 $insertTodoStatement->closeCursor();
 
--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php
 require_once('db_connect.php');
-$queryAllTodos = 'SELECT * FROM todoitems ORDER BY itemnum';
-$getTodosStatement = $db->prepare($queryAllTodos);
-$getTodosStatement->execute();
-$allTodos = $getTodosStatement->fetchAll();
-$getTodosStatement->closeCursor();
+
+if (!isset($categoryId)) {
+    $categoryId = filter_input(
+        INPUT_GET,
+        'categoryId',
+        FILTER_VALIDATE_INT
+    );
+}
+
+if ($categoryId == NULL || $categoryId == FALSE) {
+    $queryAllTodos = 'SELECT * FROM todoitems ORDER BY itemnum';
+    $getTodosStatement = $db->prepare($queryAllTodos);
+    $getTodosStatement->execute();
+    $todos = $getTodosStatement->fetchAll();
+    $getTodosStatement->closeCursor();
+} else {
+    $querySelectTodos = 'SELECT * FROM todoitems WHERE categoryID = :categoryId ORDER BY itemnum';
+    $getTodosStatement = $db->prepare($querySelectTodos);
+    $getTodosStatement->bindValue(':categoryId', $categoryId);
+    $getTodosStatement->execute();
+    $todos = $getTodosStatement->fetchAll();
+    $getTodosStatement->closeCursor();
+}
+
+$queryAllCategories = 'SELECT * FROM categories ORDER BY categoryID';
+$getCategoriesStatement = $db->prepare($queryAllCategories);
+$getCategoriesStatement->execute();
+$allCategories = $getCategoriesStatement->fetchAll();
+$getCategoriesStatement->closeCursor();
+
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +47,7 @@ $getTodosStatement->closeCursor();
             font-family: "Noto Sans", sans-serif;
         }
     </style>
-    <title>Week 4: ToDo List</title>
+    <title>Week 5: ToDo List with Categories</title>
 </head>
 
 <body>
@@ -36,20 +61,45 @@ $getTodosStatement->closeCursor();
         </nav>
     </header>
     <main class="container">
+        <section class="mt-2 mb-2">
+            <form action="index.php" method="GET">
+                <label for="categoryId">
+                    <h4>Category:</h4>
+                </label>
+                <select id="categoryId" name="categoryId">
+                    <option value="all">View All Categories</option>
+                    <?php foreach ($allCategories as $category) { ?>
+                        <option value="<?php echo $category['categoryID'] ?>">
+                            <?php echo $category['categoryName'] ?>
+                        </option>
+                    <?php } ?>
+                </select>
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </section>
+        </form>
         <section>
-            <?php if (empty($allTodos)) { ?>
-
+            <?php if (empty($todos)) { ?>
                 <p>Sorry. No todo list items exist yet.</p>
             <?php } else { ?>
                 <ul class="list-group">
-                    <?php foreach ($allTodos as $todo) { ?>
+                    <?php foreach ($todos as $todo) {
+                        $thisCategoryId = $todo['categoryID'];
+                        $queryCategoryName = 'SELECT categoryName FROM categories WHERE categoryID = :categoryId';
+                        $getCategoryNameStatement = $db->prepare($queryCategoryName);
+                        $getCategoryNameStatement->bindValue(':categoryId', $thisCategoryId);
+                        $getCategoryNameStatement->execute();
+                        $thisCategoryNameArray = $getCategoryNameStatement->fetch();
+                        $thisCategoryName = $thisCategoryNameArray['categoryName'];
+                    ?>
                         <li class="list-group-item">
                             <div class="row">
                                 <div class="col d-flex justify-content-start align-items-center">
                                     <h6 class="me-2 mb-0"><?php echo $todo['Title'] ?></h6>
                                     <p class="mb-0 text-muted"><?php echo $todo['Description'] ?></p>
                                 </div>
-                                <div class="col-3 d-flex flex-row-reverse">
+                                <div class="col-3 d-flex justify-content-between align-items-center">
+
+                                    <p class="mb-0"><?php echo $thisCategoryName; ?></p>
                                     <form action="delete_todo.php" method="POST">
                                         <input type="hidden" name="todo_itemnum" value="<?php echo $todo['ItemNum']; ?>">
                                         <button class="btn-close" aria-label="Delete"></button>
@@ -64,6 +114,16 @@ $getTodosStatement->closeCursor();
         <section>
             <h4 class="mt-2">Add Item</h4>
             <form action="add_todo.php" method="POST">
+                <label for="new_todo_categoryId">
+                    Category:
+                </label>
+                <select id="new_todo_categoryId" name="new_todo_categoryId">
+                    <?php foreach ($allCategories as $category) { ?>
+                        <option value="<?php echo $category['categoryID'] ?>">
+                            <?php echo $category['categoryName'] ?>
+                        </option>
+                    <?php } ?>
+                </select>
                 <input class="form-control m-1" type="text" name="new_todo_title" placeholder="Title">
                 <input class="form-control m-1" type="text" name="new_todo_description" placeholder="Description">
                 <button class="btn btn-primary m-1">Add Item</button>
